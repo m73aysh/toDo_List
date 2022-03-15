d0442b5a7ac459cd261375571d9533c2f09254e8
m73aysh <m7_3aysh@hotmail.com>
2022-03-15 17:16:05 +0300

    finished get and post

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,38 @@ const app = express()
 
 const db = require ("./db")
 const Todo = require ("./todo")
-console.log(Todo)
+app.use(express.json())
+
 
 app.get("/" , (req , res) => {
     res.json("GET / is working")
 })
 
 
+
 app.get("/tasks" , (req , res) => {
-    res.json("GET / is working")
+    Todo.find ({}, (err , data) => {
+        if (err) {
+            console.log( "ERROR: " , err )
+        }else{
+            res.json(data)
+        }
+    })
+})
+
+
+app.post("/newtask" , (req , res) => {
+    Todo.create (req.body , (err , newTask) => {
+        console.log(req.body)
+        if (err) {
+            console.log( "ERROR: " , err )
+        }else{
+            res.status(201).json(newTask)
+        }
+    })
 })
 
 
 app.listen (5000 , () => {
-    console.log(" Server is working...")
+    console.log(" Server is working ...")
 })
